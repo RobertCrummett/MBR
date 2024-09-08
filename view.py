78b8afb0fec7528395717a9f388f263c590a9190
Robert Crummett <rncrummett5@gmail.com>
2024-09-08 12:37:34 -0600

    Added figure text file, and plotting figure

--- view.py
+++ view.py
@@ -1,4 +1,5 @@
 from matplotlib import pyplot as plt
+plt.style.use("dark_background")
 
 class Point:
 	def __init__(self, x, y):
@@ -29,12 +30,15 @@ rect.append(rect[0])
 
 fig, ax = plt.subplots()
 
-ax.scatter([p.x for p in points], [p.y for p in points], s=30, c='r')
-ax.plot([p.x for p in hull], [p.y for p in hull], '-b')
-ax.plot([p.x for p in rect], [p.y for p in rect], '-g')
+ax.scatter([p.x for p in points], [p.y for p in points], s=30, label = "Obs. Data", color = "C0")
+ax.plot([p.x for p in hull], [p.y for p in hull], label = "Convex Hull", color = "C1")
+ax.plot([p.x for p in rect], [p.y for p in rect], label = "Min. Area Rect.", color = "C2")
 
-for i, p in enumerate(rect[:-1]):
-	ax.text(p.x, p.y, str(i))
+# for i, p in enumerate(rect[:-1]):
+#	ax.text(p.x, p.y, str(i))
 
-plt.show()
+ax.set_ylim([min((r.y for r in rect)) - 25, max((r.y for r in rect))])
+ax.legend(loc =	"lower center")
+ax.axis("off")
+plt.savefig("mbr.png", dpi=600)
 
