f46aab9cd4fced85b4d41fa025c83c3537179ac7
RobertCrummett <rncrummett5@gmail.com>
2024-09-07 23:31:46 -0600

    Beginning rotating calipers

--- hull.cpp
+++ hull.cpp
@@ -1,11 +1,11 @@
 #include <algorithm>
+#include <cmath>
 #include <iostream>
 #include <iterator>
 #include <random>
 #include <vector>
 
-typedef int coord_t;
-typedef int coord2_t;
+typedef double coord_t;
 
 struct Point {
 	coord_t x, y;
@@ -15,8 +15,18 @@ struct Point {
 	}
 };
 
+struct Rectangle {
+	Point p1, p2, p3, p4;
+
+	coord_t area() const {
+		return abs(p1.x * (p2.y - p3.y) + 
+				p2.x * (p3.y - p1.y) + 
+				p3.x * (p1.y - p2.y));
+	}
+};
+
 // 3D cross product between OA and OB
-coord2_t cross(const Point &O, const Point &A, const Point &B) {
+coord_t cross(const Point &O, const Point &A, const Point &B) {
 	return (A.x - O.x) * (B.y - O.y) - (A.y - O.y) * (B.x - O.x);
 }
 
@@ -53,20 +63,24 @@ std::vector<Point> convex_hull(std::vector<Point> points) {
 	return hull;
 }
 
-
+// Rotating callipers minimum bounding rectangle
+void rotating_calipers(std::vector<Point> hull) {
+		
+}
 
 int main() {
+	const size_t vsize = 10;
+
 	std::random_device rnd_device;
 
 	std::mt19937 mersenne_engine {rnd_device()};
-	std::uniform_int_distribution<int> dist {1, 52};
+	std::uniform_real_distribution<coord_t> dist {1.0, 52.0};
 
 	auto gen = [&](){
 		return dist(mersenne_engine);
 	};
 
-	const size_t vsize = 10;
-	std::vector<int> vx(vsize), vy(vsize);
+	std::vector<coord_t> vx(vsize), vy(vsize);
 	std::generate(vx.begin(), vx.end(), gen);
 	std::generate(vy.begin(), vy.end(), gen);
 
